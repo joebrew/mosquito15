470344aadabd14926ab4057e58d3ec5098a6acb9
joebrew <joebrew@gmail.com>
2015-05-26 17:54:22 -0400

    cleaned up spreadsheets, and cleaner code

--- forecast.R
+++ forecast.R
@@ -1,194 +1,22 @@
-library(plyr)
-library(dplyr)
-library(weatherData)
-library(randomForest)
-
-#####
-# DEFINE WORKING DIRECTORIES
-#####
-proj_root <- getwd()
-trap_dir <- paste0(proj_root, "/trap_data")
-weather_dir <- paste0(proj_root, "/weather_data")
-
-setwd(trap_dir)
-
-####
-# READ IN ALL DATA INTO LIST
-####
-temp = list.files(pattern="*.csv")
-myfiles = lapply(temp, read.csv, skip=1)
-myfileswithdate = lapply(temp, read.csv, skip=0)
-
-####
-# MAKE A NUMBER OF TRAPS COLUMN
-####
-for (i in 1:length(myfiles)){
-  myfiles[[i]]$nTraps <-
-    ncol(myfiles[[i]]) - 2
-}
-
-####
-#ADD DATE COLUMN
-####
-for (i in 1:length(myfiles)){
-  myfiles[[i]]$date <- 
-    as.Date(gsub("X", "",colnames(myfileswithdate[[i]])[1]),
-            format="%m.%d.%Y")
-}
-
-#####
-# FIND ANY MISMATCH BETWEEN FILE NAME AND FIRST ROW DATE
-#####
-mismatches <- rep(FALSE, length(myfiles))
-for (i in 1:length(myfiles)){
-  
-  # date from filename
-  date_name <- 
-    as.Date(gsub('.csv', '', temp[i]), format = '%b%d%Y')
-  
-  # date from first row
-  date_row <- 
-    myfiles[[i]]$date[1]
-  
-  # Fix mismatch
-  if( date_name != date_row ){
-    mismatches[i] <- TRUE
-  }
-}
-  
-# Which are mismatches?
-temp[mismatches]
-table(mismatches)
-
-####
-#CBIND ALL ELEMENTS OF THE MYFILES LIST INTO ONE DF
-####
-raw_data <- rbind.fill(myfiles)
-
-####
-#CODE ALL "OTHER" MOSQUITOES AS 100
-####
-raw_data$code[which(grepl("other", 
-                         tolower(raw_data$mosquito)))] <-  100
-
-#####
-# MAKE A TIME SERIES DATAFRAME
-#####
-ts <- raw_data %>%
-  group_by(date) %>%
-  summarise(n_traps = mean(nTraps, na.rm = TRUE),
-            tot = 
-              sum(site1, na.rm = TRUE) + 
-              sum(site2, na.rm = TRUE) + 
-              sum(site3, na.rm = TRUE) +
-              sum(site4, na.rm = TRUE) +
-              sum(site5, na.rm = TRUE) +
-              sum(site6, na.rm = TRUE) +
-              sum(site7, na.rm = TRUE) +
-              sum(site8, na.rm = TRUE) +
-              sum(site9, na.rm = TRUE) +
-              sum(site10, na.rm = TRUE))
-
-# ! BUG
-ts$n_traps[which(ts$n_traps > 10 )] <- 10
-
-############
-
-#####
-# GET RAINFALL DATA
-#####
-
-setwd(weather_dir)
-if(file.exists('weather.csv')){
-  weather <- read.csv('weather.csv')
-} else {
-  
-  weather_list <- list()
-  min_year <- as.numeric(format(min(ts$date), '%Y'))
-  max_year <- as.numeric(format(Sys.Date(), '%Y'))
-  year_seq <- min_year:max_year
-  for (i in 1:length(year_seq) ){
-    
-    # Define end dates
-    if(year_seq[i] == max_year){
-      end <-  Sys.Date() - 1
-    } else {
-      end <-  paste0(year_seq[i], '-12-31')
-    }
-    
-    # Get data
-    rf <- getSummarizedWeather("GNV", 
-                               start_date = paste0(year_seq[i], '-01-01'), 
-                               end_date = end,
-                               opt_all_columns = TRUE)
-    
-    rf$Date <- as.character(rf$Date)
-    
-    # Put into list
-    weather_list[[i]] <- rf
-    print(year_seq[i])
-  }
-  
-  # Combine all weather into one dataframe
-  weather <- rbind.fill(weather_list)
-  rm(min_year, max_year, year_seq, i, weather_list, rf)
-  
-  # Write csv
-  setwd(weather_dir)
-  write.csv(weather, 'weather.csv', row.names = FALSE)
-  weather <- read.csv('weather.csv')
-}
-
-# Format date
-weather$Date <- as.Date(weather$Date)
-# 
-# #####
-# # GET MORE RECENT WEATHER IF NEEDED
-# #####
-# if(max(weather$Date) < (Sys.Date() - 1)){
-#   new_weather <- getSummarizedWeather("GNV", 
-#                                       start_date = max(weather$Date) +1, 
-#                                       end_date = Sys.Date() - 1,
-#                                       opt_all_columns = TRUE)
-#   weather <- rbind(weather, new_weather)
-#   
-#   # Clean up
-#   # Format date
-#   weather$Date <- as.Date(weather$Date)
-#   
-#   
-#   # Write csv
-#   setwd(weather_dir)
-#   write.csv(weather, 'weather.csv', row.names = FALSE)
-#   weather <- read.csv('weather.csv')
-# }
-
-#####
-# CLEAN UP A BIT MORE
-#####
-
-# Format date
-weather$Date <- as.Date(weather$Date)
-
-# Order
-weather <- weather[order(weather$Date),]
-
-# Fix precipitation 
-weather$PrecipitationIn <- as.numeric(as.character(weather$PrecipitationIn))
-weather$PrecipitationIn[which(is.na(weather$PrecipitationIn))] <- 0
-
 # YONI TIME!
+source('read_in.R')
 
 # Number of mosquitoes observed at previous collection
 ts$previous_mosquitoes <- 
-  c(NA, ts$tot[1:(nrow(ts) - 1)])
+  c(NA, ts$n[1:(nrow(ts) - 1)])
 
 
 # Join weather and ts
-ts <- inner_join(ts, weather, by = c('date' = 'Date'))
+ts <- inner_join(x = ts, 
+                 y = weather[,c('date', 
+                                'Mean_TemperatureF', 
+                                'PrecipitationIn',
+                                'Min_TemperatureF', 
+                                'Max_TemperatureF')], 
+                 by = 'date')
 
 # Make a mosquitoes per trap var
-ts$y <- ts$tot / ts$n_traps
+ts$y <- ts$n / ts$n_traps
 
 # Make a previous mosquitoes per trap
 ts$previous_y <- 
@@ -196,17 +24,36 @@ ts$previous_y <-
 
 # Make a training dataset
 train <- ts[-1,c('y', 'Mean_TemperatureF', 'PrecipitationIn',
-               'Min_TemperatureF', 'Max_TemperatureF', 'previous_y')]
+                 'Min_TemperatureF', 'Max_TemperatureF', 'previous_y')]
 
-# Quick and easy model
+###############
+# Random forest
 fit <- randomForest(y ~ previous_y,
                     data = train)
 
 summary(fit)
-plot(predict(fit), train$y)
+plot(predict(fit), 
+     train$y,
+     xlab = 'Predicted',
+     ylab = 'Observed')
 
 fake <- data.frame(previous_y = 1:2000)
 plot(fake$previous_y, predict(fit, fake),)
+
+###############
+# Linear model
+fit <- lm(y ~ previous_y, 
+          data = train)
+
+summary(fit)
+plot(predict(fit), 
+     train$y,
+     xlab = 'Predicted',
+     ylab = 'Observed')
+
+fake <- data.frame(previous_y = 1:2000)
+plot(fake$previous_y, predict(fit, fake),)
+
 
 # Previous days max/min/avg temp and precipitation
 
@@ -242,9 +89,9 @@ for (i in 1:nrow(weather_augmented)){
   for (column in colnames(weather)[2:4]){
     for (day in 14:30){
       if(i > 30){
-      date <- weather_augmented$Date[i]
-      val <- weather_augmented[(i-day),column]
-      weather_augmented[i,paste0(column, '_minus_', day)] <- val
+        date <- weather_augmented$Date[i]
+        val <- weather_augmented[(i-day),column]
+        weather_augmented[i,paste0(column, '_minus_', day)] <- val
       }
     }
   }
@@ -388,4 +235,3 @@ legend('topright',
                   'Observed',
                   'Today'),
        cex = 0.8)
-
--- read_in.R
+++ read_in.R
@@ -1,15 +1,254 @@
-library(gdata)
-library(xlsx)
+library(plyr)
+library(dplyr)
+library(tidyr)
+library(weatherData)
+library(randomForest)
 
 #####
-# Set working directory to the original spreadsheets
+# DEFINE WORKING DIRECTORIES
 #####
-setwd('original_spreadsheets/')
+proj_root <- getwd()
+trap_dir <- paste0(proj_root, "/trap_data")
+weather_dir <- paste0(proj_root, "/weather_data")
+
+setwd(trap_dir)
+
+####
+# READ IN ALL DATA INTO LIST
+####
+csvs = list.files(pattern="*.csv")
+dfs <- list()
+
+for (i in 1:length(csvs)){
+  # Get date
+  date <- gsub('.csv', '', csvs[i])
+  # Read in, skipping first line
+  temp <- read.csv(csvs[i], 
+                   skip = 1,
+                   stringsAsFactors = FALSE)
+  # Get rid of blank columns
+  temp <- data.frame(temp[,!names(temp) %in% c('X', paste0('X.', 1:10))])
+  # Clean up column names
+  temp
+  # Make date column
+  temp$date <- date
+  # Make number of traps column
+  temp$n_traps <- length(which(grepl('X', names(temp))))
+  # Get dataframe into list  
+  dfs[[i]] <- temp
+}
+
+####
+#CBIND ALL ELEMENTS OF THE MYFILES LIST INTO ONE DF
+####
+raw_data <- rbind.fill(dfs)
+
+# Remove unecessary garbage
+rm(temp, csvs, date, dfs, i)
+
+# Clean up column names in raw_data
+names(raw_data) <- gsub('X', 'trap', names(raw_data))
+names(raw_data)[which(!grepl('trap', names(raw_data)))] <- 
+  c('mosquito', 'total', 'code', 'date')
+
+# Arrange column names
+raw_data <- raw_data[,sort(names(raw_data))]
+
+####
+# FIX SOME CODES
+####
+
+# Others = 100
+raw_data$code[which(grepl("other", 
+                         tolower(raw_data$mosquito)))] <-  100
+
+# Canadensis = 200
+raw_data$code[which(grepl('canad',
+                          tolower(raw_data$mosquito)))] <- 200
 
 #####
-# Read in each year's data
+# CLEAN UP
 #####
-year <- as.character(2008)
-setwd(year)
-april08 <- read.xlsx("2008 April Trap Data Sheet.xls", 1)
 
+# Populate NAs with 0s if the trap existed at the time
+for (i in 1:nrow(raw_data)){
+  n_traps <- raw_data$n_traps[i]
+    columns <- paste0('trap', 1:n_traps)
+    for (j in 1:length(columns)){
+      val <- raw_data[i, columns[j]]
+      if(is.na(val)){
+        raw_data[i, columns[j]] <- 0
+    }
+  }
+}
+
+# Clean up format a bit
+raw_data$mosquito <- NULL
+raw_data$total <- NULL
+
+# "Gather data" by a new variable named "trap", 
+#create a new variable named n (value)
+# and gather variables trap1 through trap9
+raw_data <- raw_data %>%
+  gather(key = trap, 
+         value =  n, 
+         trap1:trap9)
+
+# Clean up trap column
+raw_data$trap <- as.numeric(gsub('trap', '', raw_data$trap))
+
+# Get mosquito type
+diseases <- read.csv(paste0(proj_root, '/diseases/id.csv'))
+raw_data <- left_join(x = raw_data,
+                y = diseases[,c('code', 'name')])
+
+# Format date
+raw_data$date <- as.Date(raw_data$date)
+
+#####
+# MAKE AN AGGREGATED TIME SERIES DATAFRAME
+#####
+ts <- raw_data %>%
+  group_by(date) %>%
+  summarise(n = sum(n, na.rm = TRUE),
+            n_traps = mean(n_traps))
+
+# Remove some junk
+rm(columns, i, j, n_traps, val)
+############
+
+#####
+# GET RAINFALL DATA
+#####
+
+setwd(weather_dir)
+if(file.exists('weather.csv')){
+  weather <- read.csv('weather.csv')
+} else {
+  
+  weather_list <- list()
+  min_year <- as.numeric(format(min(ts$date), '%Y'))
+  max_year <- as.numeric(format(Sys.Date(), '%Y'))
+  year_seq <- min_year:max_year
+  for (i in 1:length(year_seq) ){
+    
+    # Define end dates
+    if(year_seq[i] == max_year){
+      end <-  Sys.Date() - 1
+    } else {
+      end <-  paste0(year_seq[i], '-12-31')
+    }
+    
+    # Get data
+    rf <- getSummarizedWeather("GNV", 
+                               start_date = paste0(year_seq[i], '-01-01'), 
+                               end_date = end,
+                               opt_all_columns = TRUE)
+    
+    rf$date <- as.character(rf$Date)
+    rf$Date <- NULL
+    # Put into list
+    weather_list[[i]] <- rf
+    print(year_seq[i])
+  }
+  
+  # Combine all weather into one dataframe
+  weather <- rbind.fill(weather_list)
+  rm(min_year, max_year, year_seq, i, weather_list, rf)
+  
+  # Write csv
+  setwd(weather_dir)
+  write.csv(weather, 'weather.csv', row.names = FALSE)
+  weather <- read.csv('weather.csv')
+}
+
+# Format date
+weather$date <- as.Date(weather$date)
+
+
+#####
+# GET MORE RECENT WEATHER IF NEEDED
+#####
+if(max(weather$date) < (Sys.Date() - 1)){
+  new_weather <- getSummarizedWeather("GNV", 
+                                      start_date = max(weather$date) +1, 
+                                      end_date = Sys.Date() - 1,
+                                      opt_all_columns = TRUE)
+  new_weather$date <- as.Date(new_weather$Date)
+  new_weather$Date <- NULL
+  weather <- rbind.fill(weather, new_weather)
+  
+  # Clean up
+  # Format date
+  weather$Date <- as.Date(weather$Date)
+  
+  
+  # Write csv
+  setwd(weather_dir)
+  write.csv(weather, 'weather.csv', row.names = FALSE)
+  weather <- read.csv('weather.csv')
+}
+
+
+# Clean up weather a bit more
+
+# Fix precipitation 
+weather$PrecipitationIn <- suppressWarnings(
+  as.numeric(as.character(weather$PrecipitationIn)))
+weather$PrecipitationIn[which(is.na(weather$PrecipitationIn))] <- 0
+
+# At this point you've got 
+#### diseases: a simple dictionary of which mosquitoes carry which diseases
+#### raw_data: number of mosquitoes by date, mosquito type, and trap
+#### ts: time series of total mosquitoes (independent of type/trap) for each date
+#### weather: daily weather observations
+
+# Pretty plot
+par(mfrow = c(2,2))
+plot(ts$date, ts$n / ts$n_traps, 
+     xlab = 'Date',
+     ylab = 'Mosquitoes per trap',
+     typ = 'l',
+     col = adjustcolor('darkgreen', alpha.f = 0.6))
+title(main = 'Skeeters')
+
+plot(weather$date, weather$Max_TemperatureF,
+     xlab = 'Date',
+     ylab = 'Temperature (farenheit)',
+     ylim = c(0, 110),
+     type = 'l',
+     col = adjustcolor('darkred', alpha.f = 0.6))
+lines(weather$date, weather$Min_TemperatureF,
+      col = adjustcolor('darkblue', alpha.f = 0.6))
+legend('bottom',
+       col = adjustcolor(c('darkred', 'darkblue'), alpha.f = 0.6),
+       legend = c('Maximum', 'Minimum'), 
+       lty = 1,
+       bty = 'n',
+       cex = 0.7)
+title(main = 'Temperature')
+
+weather$week <- as.numeric(format(weather$date, '%U'))
+temp <- weather %>%
+  group_by(week) %>%
+  summarise(rain = sum(PrecipitationIn),
+            humidity = mean(Mean_Humidity))
+plot(temp$week, temp$rain,
+     xlab = 'Week',
+     ylab = 'Inches of rain',
+     main = 'Precipitation',
+     type = 'l',
+     col = adjustcolor('darkorange', alpha.f = 0.6),
+     ylim = c(0, 30))
+
+plot(temp$week, temp$humidity,
+     xlab = 'Week',
+     ylab = 'Mean humidity',
+     main = 'Humidity',
+     type = 'l',
+     col = adjustcolor('purple', alpha.f = 0.6),
+     ylim = c(60, 100))
+weather$week <- NULL
+
+
+par(mfrow = c(1,1))
